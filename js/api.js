e5a12325735b90116fc1f7ff58555c258c5641be
saeleuterio <saeleuterio@hotmail.com>
2025-02-06 10:51:51 -0300

    up

--- js/api.js
+++ js/api.js
@@ -1,33 +1,60 @@
 const api = {
-    async buscarPensamentos () {
-        try {
-            const response = await fetch('http://localhost:3000/pensamentos')
-            return await response.json()
-        }
-        catch {
-            alert('Erro ao buscar pensamentos')
-            throw error
-        }
+    async buscarPensamentos() {
+      try {
+        const response = await fetch('http://localhost:3000/pensamentos')
+        return await response.json()
+      }
+      catch {
+        alert('Erro ao buscar pensamentos')
+        throw error
+      }
     },
-
-    async salvarPensamentos (pensamento) {
-        try {
-            const response = await fetch('http://localhost:3000/pensamentos', {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(pensamento)
-            })
-            return await response.json()
-        }
-        catch {
-            alert('Erro ao buscar pensamentos')
-            throw error
-        }
-    }
-
-
-}
-
-export default api;
+  
+    async salvarPensamento(pensamento) {
+      try {
+        const response = await fetch('http://localhost:3000/pensamentos', {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(pensamento)
+        })
+        return await response.json()
+      }
+      catch {
+        alert('Erro ao salvar pensamento')
+        throw error
+      }
+    },
+  
+    async buscarPensamentoPorId(id) {
+      try {
+        const response = await fetch(`http://localhost:3000/pensamentos/${id}`)
+        return await response.json()
+      }
+      catch {
+        alert('Erro ao buscar pensamento')
+        throw error
+      }
+    },
+  
+    async editarPensamento(pensamento) {
+      try {
+        const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(pensamento)
+        })
+        return await response.json()
+      }
+      catch {
+        alert('Erro ao editar pensamento')
+        throw error
+      }
+    },
+    
+  }
+  
+  export default api
--- js/ui.js
+++ js/ui.js
@@ -1,47 +1,68 @@
 import api from "./api.js"
 
 const ui = {
-    limparFormulario() {
-        document.getElementById("pensamento-form").reset();
-    },
+
+  async preencherFormulario(pensamentoId) {
+    const pensamento = await api.buscarPensamentoPorId(pensamentoId)
+    document.getElementById("pensamento-id").value = pensamento.id
+    document.getElementById("pensamento-conteudo").value = pensamento.conteudo
+    document.getElementById("pensamento-autoria").value = pensamento.autoria
+  },
+
+  limparFormulario() {
+    document.getElementById("pensamento-form").reset();
+  },
   
-    async renderizarPensamentos() {
-        const listaPensamentos = document.getElementById("lista-pensamentos")
-        
-        try {
-            const pensamentos = await api.buscarPensamentos()
-            pensamentos.forEach(ui.adicionarPensamentoNaLista)
-        }
-        catch {
-            alert('Erro ao renderizar pensamentos')
-        }
-    },
+  async renderizarPensamentos() {
+    const listaPensamentos = document.getElementById("lista-pensamentos")
 
-    adicionarPensamentoNaLista (pensamento) {
-        const listaPensamentos = document.getElementById("lista-pensamentos")
-        const li = document.createElement("li")
-        li.setAttribute("data-id", pensamento.id)
-        li.classList.add("li-pensamento")
-
-        const iconeAspas = document.createElement("img")
-        iconeAspas.src = "assets/imagens/aspas-azuis.png"
-        iconeAspas.alt = "Aspas azuis"
-        iconeAspas.classList.add("icone-aspas")
-
-        const pensamentoConteudo = document.createElement("div")
-        pensamentoConteudo.textContent = pensamento.conteudo
-        pensamentoConteudo.classList.add("pensamento-conteudo")
-
-        const pensamentoAutoria = document.createElement("div")
-        pensamentoAutoria.textContent = pensamento.autoria
-        pensamentoAutoria.classList.add("pensamento-autoria")
-
-        li.appendChild(iconeAspas)
-        li.appendChild(pensamentoConteudo)
-        li.appendChild(pensamentoAutoria)
-        listaPensamentos.appendChild(li)
-
+    try {
+      const pensamentos = await api.buscarPensamentos()
+      pensamentos.forEach(ui.adicionarPensamentoNaLista)
     }
+    catch {
+      alert('Erro ao renderizar pensamentos')
+    }
+  },
+
+  adicionarPensamentoNaLista(pensamento) {
+    const listaPensamentos = document.getElementById("lista-pensamentos")
+    const li = document.createElement("li")
+    li.setAttribute("data-id", pensamento.id)
+    li.classList.add("li-pensamento")
+
+    const iconeAspas = document.createElement("img")
+    iconeAspas.src = "assets/imagens/aspas-azuis.png"
+    iconeAspas.alt = "Aspas azuis"
+    iconeAspas.classList.add("icone-aspas")
+
+    const pensamentoConteudo = document.createElement("div")
+    pensamentoConteudo.textContent = pensamento.conteudo
+    pensamentoConteudo.classList.add("pensamento-conteudo")
+
+    const pensamentoAutoria = document.createElement("div")
+    pensamentoAutoria.textContent = pensamento.autoria
+    pensamentoAutoria.classList.add("pensamento-autoria")
+
+    const botaoEditar = document.createElement("button")
+    botaoEditar.classList.add("botao-editar")
+    botaoEditar.onclick = () => ui.preencherFormulario(pensamento.id)
+
+    const iconeEditar = document.createElement("img")
+    iconeEditar.src = "assets/imagens/icone-editar.png"
+    iconeEditar.alt = "Editar"
+    botaoEditar.appendChild(iconeEditar)
+
+    const icones = document.createElement("div")
+    icones.classList.add("icones")
+    icones.appendChild(botaoEditar)
+
+    li.appendChild(iconeAspas)
+    li.appendChild(pensamentoConteudo)
+    li.appendChild(pensamentoAutoria)
+    li.appendChild(icones)
+    listaPensamentos.appendChild(li)
+  }
 }
 
-export default ui;
+export default ui
